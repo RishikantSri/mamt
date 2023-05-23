92c4cf80310c8fd562bb22d52b1a7d213d8c2b7b
RishikantSri <>
2023-05-23 12:11:43 +0530

    Models, Migration, Factory, Seederd base classe created using command

---
+++ database/factories/SuperAdmin/SuperAdminFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\SuperAdmin;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SuperAdmin\SuperAdmin>
+ */
+class SuperAdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
